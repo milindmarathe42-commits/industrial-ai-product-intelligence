d191e3683a9b39e5e218a82c7788849be32fb1c5
milind1177 <milindmarathe42@gmail.com>
2026-08-08 20:14:11 +0530

    Update api.js

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 const api = axios.create({
 
-    baseURL: "http://127.0.0.1:8000"
+    baseURL: "https://industrial-ai-product-intelligence-production.up.railway.app"
 
 });
 
