b66f0fd846402716c59fd635de31dcad3b012e2d
milind1177 <milindmarathe42@gmail.com>
2026-08-08 23:32:44 +0530

    Update vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,7 +1,11 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+
+  preview: {
+    host: '0.0.0.0',
+    allowedHosts: ['remarkable-enjoyment-production-84f9.up.railway.app']
+  }
 })
